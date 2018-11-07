cf9dd077fed1f07f26fb2c2314a0aabbf9531ea5
Thea Lanherne <thea.lanherne@movegb.com>
2018-11-07 11:19:03 +0000

    Convert to typescript

--- ios/theaTests/theaTests.m
+++ ios/theaTests/theaTests.m
@@ -12,7 +12,7 @@
 #import <React/RCTRootView.h>
 
 #define TIMEOUT_SECONDS 600
-#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+#define TEXT_TO_LOOK_FOR @"Welcome to Thea's React Native App!"
 
 @interface theaTests : XCTestCase
 
