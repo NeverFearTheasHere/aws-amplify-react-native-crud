d80266f71ada988e0b9522e824c0a0228acecc5d
Thea Lanherne <thea.lanherne@movegb.com>
2018-11-08 10:17:14 +0000

    Add aws-appsync with cognito for user authentication

--- App.tsx
+++ App.tsx
@@ -1,30 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Amplify, { Auth } from 'aws-amplify';
+import { ApolloProvider } from 'react-apollo';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import AWSAppSyncClient from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import AppSyncConfig from './app-sync-config';
+import aws_exports from './aws-exports';
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+Amplify.configure(aws_exports);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const awsClient = new AWSAppSyncClient({
+  url: AppSyncConfig.graphqlEndpoint,
+  region: AppSyncConfig.region,
+  auth: {
+    type: AppSyncConfig.authenticationType,
+    jwtToken: async () =>
+      (await Auth.currentSession()).getIdToken().getJwtToken(),
+  },
 });
+const Root = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to Thea's React Native App!</Text>
+    <Text style={styles.instructions}>You have successfully logged in</Text>
+  </View>
+);
 
-type Props = {};
-export default class App extends Component<Props> {
+const RootWithAuth = withAuthenticator(Root);
+
+class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Thea's React Native App!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ApolloProvider client={awsClient}>
+        <Rehydrated>
+          <RootWithAuth />
+        </Rehydrated>
+      </ApolloProvider>
     );
   }
 }
@@ -47,3 +58,5 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+export default App;
